d0210de3850f8435f36fbc6558786cbf40985046
kparashar <p.kulbhushan11@gmail.com>
2025-10-06 18:33:10 -0700

    refactor: remove all files

--- README.md
+++
@@ -1,3 +0,0 @@
-# docker-tutorial
-This repo is for learning docker and containers
-this repo will use for kubernetes as well.
--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,9 @@
-resources "aws_instance" "web" {
-    ami           = "ami-0b5eea76982371e91"
-    instance_type = "t2.micro"
+# resources "aws_instance" "web" {
+#     ami           = "ami-0b5eea76982371e91"
+#     instance_type = "t2.micro"
 
-    tags = {
-        Name = "HelloWorld"
-        Environment = "dev"
-    }
-}
+#     tags = {
+#         Name = "HelloWorld"
+#         Environment = "dev"
+#     }
+# }
