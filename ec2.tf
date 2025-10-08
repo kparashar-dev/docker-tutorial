09591dfba5c50d16bd195668c56261cc96a2cacd
kparashar-dev <kparasharawsus26@gmail.com>
2025-10-07 22:56:49 -0700

    refactor: remove ec2

--- ec2.tf
+++
@@ -1,9 +0,0 @@
-# resources "aws_instance" "web" {
-#     ami           = "ami-0b5eea76982371e91"
-#     instance_type = "t2.micro"
-
-#     tags = {
-#         Name = "HelloWorld"
-#         Environment = "dev"
-#     }
-# }
